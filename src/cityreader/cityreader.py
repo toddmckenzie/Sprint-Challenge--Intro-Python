9ac8faa0a3a9186eee909a0b56d044d14243a9e0
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-09-06 12:03:23 -0500

    city reader working and printing out city and lat and lon, but failing test

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,7 +1,14 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City():
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
+  def __str__(self):
+    return ("City is: " + self.name + " Lat is: " + str(self.lat) + " Lon is: " + str(self.lon))
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -16,11 +23,20 @@
 # should not be loaded into a City object.
 cities = []
 
+import csv
+
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    with open('cities.csv') as csvfile:
+      for i in csvfile:
+        createdList = i.split(",")
+        if i[0:4] != "city":
+         cities.append(City(createdList[0], createdList[3], createdList[4]))
+      
+      
+      
     return cities
 
 cityreader(cities)
